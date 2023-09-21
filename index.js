6f301ae4ff602f409b934a726e4cdcb11572da8a
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-09-21 19:10:55 +0430

    branch created

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
+function miniMaxSum(arr) {
+    // Write your code here
 
-
-function staircase(n) {
-    let stair = "";
-    for (let i = 0; i < n; i++) {
-        stair = "";
-        for (let j = 0; j < n; j++) {
-            if (j < n - i - 1) {
-                stair += " ";
-            } else {
-                stair += "#";
-            }
-        }
-        console.log(stair);
-    }
 }
-staircase(4);
