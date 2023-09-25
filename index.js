eef2a2fc578c98a43a07a68708da3442e509ea48
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-09-25 13:28:19 +0430

    solution added was easy a little proud

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function miniMaxSum(arr) {
     // Write your code here
-
+ let sortedArray=   arr.sort((a,b)=>(a - b));
+   let totalMax=0;
+   for(let i=1;i<arr.length;i++){
+     totalMax+=arr[i];
+   }
+   let totalMin = 0;
+   for(let j=0;j<arr.length-1;j++){
+    totalMin+=arr[j];
+   }
+console.log(totalMin,totalMax);
 }
+miniMaxSum([3,2,9,1,9])
