d4b493992ccc648d050769ed92625ab1275d5066
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-02 15:31:44 +0430

    solved a little bit proud well done boy

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 /*
- * Complete the 'compareTriplets' function below.
+ * Complete the 'gradingStudents' function below.
  *
  * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
+ * The function accepts INTEGER_ARRAY grades as parameter.
  */
 
-function compareTriplets(a, b) {
-  let aliceScorre = 0;
-  let bobScore = 0;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] > b[i]) {
-      aliceScorre++;
-    } else if (b[i] > a[i]) {
-      bobScore++;
+function gradingStudents(grades) {
+  let roundUpArray = [];
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > 37) {
+      if (grades[i] % 5 > 2) {
+        roundUpArray.push(grades[i] + (5 - (grades[i] % 5)))
+      } else {
+        roundUpArray.push(grades[i]);
+      }
+    } else {
+      roundUpArray.push(grades[i])
     }
   }
-  // if(aliceScorre===0&&bobScore===0){
-  //   return [];
-  // }
-  return [aliceScorre, bobScore];
+  return roundUpArray;
+
 }
+
+let testArray = [73, 67, 38, 33];
+console.log(testArray);
+console.log(gradingStudents(testArray));
