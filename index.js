98d5f962e1b43c8dfbf24c85f8ee489b541bc0cd
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-21 03:51:55 +0430

    initial commit

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-function mergeSortArray(arr1, arr2) {
-    if (arr1.length === 0) {
-        return arr2
+class HashTable{
+    constructor(size){
+        this.data=new Array(size)
     }
-
-    if (arr2.length === 0) {
-        return arr1
-    }
-    let i = 0;
-    let j = 0;
-    let newMergedArray = []
-
-    while (arr1.length > i && arr2.length > j) {
-        if (arr1.length == i && arr2.length == j) {
-            return newMergedArray
+    _hash(key){
+        let hash=0;
+        for(let i=0;i<key.length;i++){
+            hash=(hash+key.charCodeAt(i)*i)% this.data.length
         }
-        if (arr1[i] < arr2[j]) {
-            newMergedArray.push(arr1[i])
-            i++
-        } else {
-            newMergedArray.push(arr2[j])
-            j++
-        }
+        return hash;
     }
-    if (arr2.length == j) {
-        for (let k = i; k < arr1.length; k++) {
-            newMergedArray.push(arr1[k])
-        }
-    }
-    if (arr1.length == i) {
-        for (let p = j; p < arr2.length; p++) {
-            newMergedArray.push(arr2[p])
-        }
-    }
-
-    return newMergedArray
-
 }
-console.log(mergeSortArray([4, 5, 8, 33, 99], [3, 3, 4, 5, 8, 8, 8, 9, 9, 9, 98]));
-console.log(mergeSortArray([8, 9, 11], []));
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes',10000);
+myHashTable.get('grapes'); 
