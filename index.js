8bf353424e5fd5b5628cfd296a1e894e71c35f09
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-21 09:43:16 +0430

    solved using hash in javascript

--- index.js
+++ index.js
@@ -10,14 +10,16 @@
 
 
 function firstRecurringCharacter(input) {
-    for (let i = 0; i < input.length; i++) {
-        for (let j = 0; j < input.length; j++) {
-            if (i !== j && input[i] === input[j]) {
-                return input[i]
-            }
-        }
-        return undefined
+ let map={}
+ for(let i=0;i<input.length;i++){
+    if(map[input[i]]!== undefined){
+        return input[i]
+    }else {
+        map[input[i]]=true
     }
+ }
+ return undefined;
+
 }
 console.log(firstRecurringCharacter([2,1,1,2,3,5,1,2,4]));
 //Bonus... What if we had this:
