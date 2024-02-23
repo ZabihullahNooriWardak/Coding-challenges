4356db28c5aa39966ac433e1a84c4cb5abb0d087
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-23 13:37:40 +0430

    solved half the way

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
 // add a method insert() to the linked list that adds a node to the specified index.
 
 class LinkedList {
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null
-      };
-      this.tail = this.head;
-      this.length = 1;
-    }
-    append(value) {
-      const newNode = {
-        value: value,
-        next: null
-      }
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-      return this;
-    }
-    prepend(value) {
-      const newNode = {
-        value: value,
-        next: null
-      }
-      newNode.next = this.head;
-      this.head = newNode;
-      this.length++;
-      return this;
-    }
-    printList() {
-      const array = [];
-      let currentNode = this.head;
-      while(currentNode !== null){
-          array.push(currentNode.value)
-          currentNode = currentNode.next
-      }
-      return array;
-    }
-    insert(index, value){
-      //Code here
-      return this.printList();
-    }
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
   }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array;
+  }
+  insert(index, value) {
+    //Code here
+    let node = {
+      value:value,
+      next:null
+    }
+    let i = 0;
+    let current = this.head;
+    while(i<=(index)){
   
-  let myLinkedList = new LinkedList(10);
-  myLinkedList.append(5);
-  myLinkedList.append(16);myLinkedList.prepend(1)
-  myLinkedList.insert(2, 99)
-  myLinkedList.insert(20, 88)
-  
-  
-  
-  
+      if(i===(index-1)){
+        current.next=node;
+
+      }else if(i<(index-1)){
+      current=current.next
+      }else if(i===index){
+        
+      }
+      i++;
+    }
+
+    return this.printList();
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16); myLinkedList.prepend(1)
+myLinkedList.insert(2, 99)
+myLinkedList.insert(20, 88)
+
+
+
