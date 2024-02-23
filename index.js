29fa53c5f61a9b5adfc6e39e09b0c5696c504f87
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-23 10:09:39 +0430

    initial commit

--- index.js
+++ index.js
@@ -1,60 +1,55 @@
-// Create the below linked list:
-// myLinkedList = {
-//   head: {
-//     value: 10
-//     next: {
-//       value: 5
-//       next: {
-//         value: 16
-//         next: null
-//       }
-//     }
-//   }
-// };
-
-// class Node{
-//     constructor(value){
-//         this.value=value;
-//         this.next=null
-//     }
-// }
+// add a method insert() to the linked list that adds a node to the specified index.
 
 class LinkedList {
     constructor(value) {
-        this.head = {
-            value: value,
-            next: null
-        };
-        this.tail = this.head;
-        this.length = 1;
+      this.head = {
+        value: value,
+        next: null
+      };
+      this.tail = this.head;
+      this.length = 1;
     }
     append(value) {
-        let node = {
-            value: value,
-            next: null
-        }
-        this.tail.next = node;
-        this.tail = node;
-        this.length++;
-        return this;
-
-    }
-    prepend(value){
-      let node = {
-        value:value,
-        next:this.head
+      const newNode = {
+        value: value,
+        next: null
       }
-      this.head=node
+      this.tail.next = newNode;
+      this.tail = newNode;
       this.length++;
+      return this;
     }
-
-}
-
-
-let myLinkedList = new LinkedList(10);
-myLinkedList.append(5);
-console.log(myLinkedList.append(16));
-//   console.log(myLinkedList);
-
-
-
+    prepend(value) {
+      const newNode = {
+        value: value,
+        next: null
+      }
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+      return this;
+    }
+    printList() {
+      const array = [];
+      let currentNode = this.head;
+      while(currentNode !== null){
+          array.push(currentNode.value)
+          currentNode = currentNode.next
+      }
+      return array;
+    }
+    insert(index, value){
+      //Code here
+      return this.printList();
+    }
+  }
+  
+  let myLinkedList = new LinkedList(10);
+  myLinkedList.append(5);
+  myLinkedList.append(16);myLinkedList.prepend(1)
+  myLinkedList.insert(2, 99)
+  myLinkedList.insert(20, 88)
+  
+  
+  
+  
