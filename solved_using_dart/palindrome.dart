0dd5dbceb0931d24248ff686f8aa3e45910985a9
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-03-22 07:29:25 +0430

    adds google solution

--- solved_using_dart/palindrome.dart
+++ solved_using_dart/palindrome.dart
@@ -1,46 +1,61 @@
 void main() {
-  print(isPalindrome("ناان"));
+  print(isPalindrome("boodb"));
 }
 
 // boob   bob
 
+// first solution (mine)
+
+// bool isPalindrome(String str) {
+//   double halfLength = str.length / 2;
+//   String firstString = "";
+//   String secondString = "";
+//   if (str.length % 2 == 0) {
+//     for (int i = 0; i < halfLength; i++) {
+//       firstString += str[i];
+//     }
+
+//     for (int i = str.length - 1; i >= halfLength; i--) {
+//       secondString += str[i];
+//     }
+//     print(firstString);
+//     print(secondString);
+//     if (firstString == secondString) {
+//       return true;
+//     }
+//   } else {
+//     var middleIndex = halfLength - 0.5;
+//     for (int i = 0; i < halfLength; i++) {
+//       if (i == middleIndex) {
+//         continue;
+//       }
+//       firstString += str[i];
+//     }
+
+//     for (int i = str.length - 1; i >= halfLength; i--) {
+//       if (i == middleIndex) {
+//         continue;
+//       }
+//       secondString += str[i];
+//     }
+//     print(firstString);
+//     print(secondString);
+//     if (firstString == secondString) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// second solution (internet google )
+
 bool isPalindrome(String str) {
-  double halfLength = str.length / 2;
-  String firstString = "";
-  String secondString = "";
-  if (str.length % 2 == 0) {
-    for (int i = 0; i < halfLength; i++) {
-      firstString += str[i];
-    }
-
-    for (int i = str.length - 1; i >= halfLength; i--) {
-      secondString += str[i];
-    }
-    print(firstString);
-    print(secondString);
-    if (firstString == secondString) {
-      return true;
-    }
-  } else {
-    var middleIndex = halfLength - 0.5;
-    for (int i = 0; i < halfLength; i++) {
-      if (i == middleIndex) {
-        continue;
-      }
-      firstString += str[i];
-    }
-
-    for (int i = str.length - 1; i >= halfLength; i--) {
-      if (i == middleIndex) {
-        continue;
-      }
-      secondString += str[i];
-    }
-    print(firstString);
-    print(secondString);
-    if (firstString == secondString) {
-      return true;
-    }
+  var newString = "";
+  for (int i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  if (str == newString) {
+    return true;
   }
   return false;
 }
