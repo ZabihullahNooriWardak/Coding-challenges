f3960efb52f45e052e9353f85746e468d3ace934
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-04-17 20:56:37 +0430

    my own solution

--- solved_using_dart/anagram.dart
+++ solved_using_dart/anagram.dart
@@ -3,7 +3,7 @@ void main(){
 
 
 }
-
+//What is the challange ? 
 /* 
 In other words, two strings are considered anagrams
  if they have the same characters with the same frequency,
@@ -12,6 +12,11 @@ In other words, two strings are considered anagrams
 */
 
 
+// my Algorithm steps 
+
+// first we can check the length of both string 
+// we should check sigle character of a string that is it in another string  and number of occurence
+// and then check the number of occurence 
 bool isAnagram(){
 
 }
