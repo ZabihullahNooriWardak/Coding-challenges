c6b55e56833617688e6d9002bdae17dfe3cc076c
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-04-17 21:33:12 +0430

    completes without test and optimization

--- solved_using_dart/anagram.dart
+++ solved_using_dart/anagram.dart
@@ -1,9 +1,5 @@
-
-void main(){
-
-
-}
-//What is the challange ? 
+void main() {}
+//What is the challange ?
 /* 
 In other words, two strings are considered anagrams
  if they have the same characters with the same frequency,
@@ -11,12 +7,41 @@ In other words, two strings are considered anagrams
    “listen” and “silent” are anagrams because you can rearrange the letters in one to form the other.
 */
 
+// my Algorithm steps
 
-// my Algorithm steps 
-
-// first we can check the length of both string 
+// first we can check the length of both string
 // we should check sigle character of a string and compare it with  another string  and number of occurence of each character
-// and then check the number of occurence 
-bool isAnagram(){
+// and then check the number of occurence
+bool isAnagram(String str1, String str2) {
+  String character1 = '';
+  String character2 = '';
+  int counter1 = 0;
+  int counter2 = 0;
+  for (int i = 0; i < str1.length; i++) {
+    character1 = str1[i];
+    counter1 = 0;
+    for (int j = 0; j < str2.length; j++) {
+      if (character1 == str2[j]) {
+        counter1++;
+      }
+    }
+    if (counter1 == 0) {
+      return false;
+    }
+  }
 
+  for (int i = 0; i < str2.length; i++) {
+    character2 = str2[i];
+    counter2 = 0;
+    for (int j = 0; j < str1.length; j++) {
+      if (character2 == str1[j]) {
+        counter2++;
+      }
+    }
+    if (counter2 == 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
