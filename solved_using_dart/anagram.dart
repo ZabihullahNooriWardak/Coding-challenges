9c1cdb4cabad64a36bb83bdfeab6345488c2b82b
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-04-18 15:43:32 +0430

    fix the edge case

--- solved_using_dart/anagram.dart
+++ solved_using_dart/anagram.dart
@@ -1,11 +1,11 @@
 void main() {
-  // print(isAnagram('listen', 'silent'));
-  // print(isAnagram('evil', 'vile'));
-  // print(isAnagram('a', 'b'));
+  print(isAnagram('listen', 'silent'));
+  print(isAnagram('evil', 'vile '));
+  print(isAnagram('a', 'b'));
 
-  var map = {'a': 3, 'b': "3333"};
-  print(map['a']);
-  print(map['d']);
+  // var map = {'a': 3, 'b': "3333"};
+  // print(map['a']);
+  // print(map['d']);
   print(isAnagram('abb', 'baa'));
 }
 //What is the challange ?
@@ -21,50 +21,75 @@ In other words, two strings are considered anagrams
 // first we can check the length of both string
 // we should check sigle character of a string and compare it with  another string  and number of occurence of each character
 // and then check the number of occurence
+// bool isAnagram(String str1, String str2) {
+//   String character1 = '';
+//   String character2 = '';
+//   int counter1 = 0;
+//   int counter2 = 0;
+//   if (str1.length != str2.length) {
+//     return false;
+//   }
+//   for (int i = 0; i < str1.length; i++) {
+//     character1 = str1[i];
+//     counter1 = 0;
+//     for (int j = 0; j < str2.length; j++) {
+//       if (character1 == str2[j]) {
+//         counter1++;
+//       }
+//     }
+//     if (counter1 == 0) {
+//       return false;
+//     }
+//   }
+
+//   for (int i = 0; i < str2.length; i++) {
+//     character2 = str2[i];
+//     counter2 = 0;
+//     for (int j = 0; j < str1.length; j++) {
+//       if (character2 == str1[j]) {
+//         counter2++;
+//       }
+//     }
+//     if (counter2 == 0) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
 bool isAnagram(String str1, String str2) {
-  String character1 = '';
-  String character2 = '';
-  int counter1 = 0;
-  int counter2 = 0;
   if (str1.length != str2.length) {
     return false;
   }
-  for (int i = 0; i < str1.length; i++) {
-    character1 = str1[i];
-    counter1 = 0;
-    for (int j = 0; j < str2.length; j++) {
-      if (character1 == str2[j]) {
-        counter1++;
-      }
-    }
-    if (counter1 == 0) {
-      return false;
-    }
-  }
 
-  for (int i = 0; i < str2.length; i++) {
-    character2 = str2[i];
-    counter2 = 0;
+  for (int i = 0; i < str1.length; i++) {
+    var character = '';
+    var occurenceCounter = 1;
+    character = str1[i];
     for (int j = 0; j < str1.length; j++) {
-      if (character2 == str1[j]) {
-        counter2++;
+      if (character == str1[j] && i != j) {
+        occurenceCounter += 1;
       }
     }
-    if (counter2 == 0) {
-      return false;
+    bool result = anotherFunc(occurenceCounter, character, str2);
+    if (result == false) {
+      return result;
     }
   }
 
   return true;
 }
 
-// bool isAnagram(String str1, String str2) {
-//   var firstMap = {};
-//   if (str1.length != str2.length) {
-//     return false;
-//   }
-//   for (int i = 0; i < str1.length;i++){
- 
-//   }
-//    return false;
-// }
+bool anotherFunc(int numberOfOccurence, String character, String second) {
+  int counter = 0;
+  for (int i = 0; i < second.length; i++) {
+    if (second[i] == character) {
+      counter++;
+    }
+  }
+  if (counter == numberOfOccurence) {
+    return true;
+  }
+  return false;
+}
