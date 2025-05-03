904b41d2cf924abbb61a7c5f943e877869663829
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-05-03 05:41:26 +0430

    new linked list challnge

--- solved_using_dart/Fresh/1-two-sum.dart
+++ solved_using_dart/Fresh/1-two-sum.dart
@@ -5,17 +5,9 @@ void main() {
 }
 
 // 1. Two Sum
-// Solved
-// Easy
-// Topics
-// Companies
-// Hint
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 // You can return the answer in any order.
-
 // Example 1:
 
 // Input: nums = [2,7,11,15], target = 9
@@ -34,9 +26,10 @@ List<int> twoSum(List<int> nums, int target) {
   List<int> newArray = [];
   for (int i = 0; i < nums.length; i++) {
     for (int j = 0; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target && i != j) {
+      if ((nums[i] + nums[j] == target) && (i != j)) {
         newArray.add(i);
         newArray.add(j);
+        return newArray;
       }
     }
   }
