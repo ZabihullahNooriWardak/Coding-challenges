4e27467874a8d7bbd6520369aee924a1395af229
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-06-10 17:45:55 +0430

    adds challange

--- solved_using_dart/Data_structures/linked_list.dart
+++ solved_using_dart/Data_structures/linked_list.dart
@@ -1,10 +1,19 @@
 // Node class
 
 void main() {
+  for (int i = 0; i < 5; i++) {
+    // print('this i is : $i');
+  }
   int x = 3;
   int y = x;
   x = 8;
   print('tis is y vluae : $y');
+  LinkedList lst = LinkedList();
+  lst.append(2);
+  lst.append(8);
+  lst.prepend(77);
+  lst.insert(2, 99);
+  lst.printList();
 }
 
 class Node {
@@ -17,9 +26,9 @@ class Node {
 // Linked List class
 class LinkedList {
   Node? head;
+// prepend node at beggining
   void prepend(int data) {
     Node newNode = Node(data);
-
     if (head == null) {
       head = newNode;
       return;
@@ -40,6 +49,26 @@ class LinkedList {
     while (current.next != null) {
       current = current.next!;
     }
+    current.next = newNode;
+  }
+
+  void insert(int index, int value) {
+    if (value < 0) {
+      return;
+    }
+    if (value == 0) {
+      prepend(value);
+      return;
+    }
+    Node current = head!;
+    for (int i = 0; i < index - 1 && current.next != null; i++) {
+      current = current.next!;
+    }
+    if (current == null) {
+      return;
+    }
+    var newNode = Node(value);
+    newNode.next = current.next;
     current.next = newNode;
   }
 
